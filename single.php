e2c984b4043906ba9f8b1461c38f20b18561394f
Eduardo Zulian <eduardozulian@gmail.com>
2014-01-13 16:25:58 -0200

    Remove estrutura do entry-meta

--- single.php
+++ single.php
@@ -35,44 +35,11 @@ get_header(); ?>
 				</div><!-- .entry-content -->
 
 				<footer class="entry-meta">
-					<?php
-						/* translators: used between list items, there is a space after the comma */
-						$category_list = get_the_category_list( __( ', ', 'quizumba' ) );
-
-						/* translators: used between list items, there is a space after the comma */
-						$tag_list = get_the_tag_list( '', __( ', ', 'quizumba' ) );
-
-						if ( ! quizumba_categorized_blog() ) {
-							// This blog only has 1 category so we just need to worry about tags in the meta text
-							if ( '' != $tag_list ) {
-								$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'quizumba' );
-							} else {
-								$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'quizumba' );
-							}
-
-						} else {
-							// But this blog has loads of categories so we should probably display them here
-							if ( '' != $tag_list ) {
-								$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'quizumba' );
-							} else {
-								$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'quizumba' );
-							}
-
-						} // end check for categories on this blog
-
-						printf(
-							$meta_text,
-							$category_list,
-							$tag_list,
-							get_permalink()
-						);
-					?>
-
-					<?php edit_post_link( __( 'Edit', 'quizumba' ), '<span class="edit-link">', '</span>' ); ?>
+					<?php quizumba_the_category_list(); ?>
+					<?php quizumba_the_tag_list(); ?>
 					<?php edit_post_link( __( 'Edit', 'quizumba' ), '<span class="edit-link icon-edit">', '</span>' ); ?>
 				</footer><!-- .entry-meta -->
 			</article><!-- #post-## -->
-
 
 			<?php quizumba_post_nav(); ?>
 
